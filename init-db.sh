ded3f2d66a081a6300f378168f8643284d008382
shin <thycaouyenhuynh331@gmail.com>
2025-11-04 04:42:54 +0700

    2025-11-04 - Config changes before backup

--- init-db.sh
+++ init-db.sh
@@ -6,8 +6,9 @@ set -e
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
     CREATE DATABASE paperless_db;
     CREATE DATABASE nextcloud_db;
+    CREATE DATABASE wallabag_db;
     -- Xóa các dòng CREATE DATABASE cho gitea, freshrss, wallabag, joplin... nếu có
 EOSQL
 
-echo "Databases paperless_db and nextcloud_db created successfully."
+echo "Databases paperless_db, nextcloud_db, and wallabag_db created successfully."
 
